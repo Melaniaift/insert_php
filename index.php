f82285aee15497785e878695034139c4d80ce393
Melaniaift <ioanamelany99@yahoo.com>
2021-07-04 20:55:30 +0300

    insert

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="paginanoua.php" method="post">
+            <input type="text" name="text1" size="10"/>
+            <br/><br/>
+            <input type="submit" value="Trimite" />
+        </form>
+    </body>
+</html>
---
+++ paginanoua.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<?php 
+echo "<h1>Ati introdus:</h1><b> ".$_POST["text1"]."</b>";
+?>
